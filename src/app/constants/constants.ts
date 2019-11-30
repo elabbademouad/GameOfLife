01af8cbb6d583d7376fc4baaa72629fa2ba2e42c
Mouad El abbade <elabbademouad@gmail.com>
2019-11-30 03:54:00 +0100

    add constants

---
+++ src/app/constants/constants.ts
@@ -0,0 +1,12 @@
+export class Constants {
+    public static DEFAULT_SPEED: number = 480;
+    public static MAX_SPEED: number = 2080;
+    public static CANVAS_WIDTH: number = 600;
+    public static CANVAS_HEIGHT: number = 600;
+    public static DEFAULT_DRAWING_UNIT: number = 10;
+    public static MIN_DRAWING_UNIT: number = 2;
+    public static DEFAULT_GRID_FILL_COLOR: string = "white";
+    public static DEFAULT_GRID_STORK_COLOR: string = "gray";
+    public static DEFAULT_SCENE_FILL_COLOR: string = "green";
+    public static DEFAULT_SCENE_STORK_COLOR: string = "black";
+}
