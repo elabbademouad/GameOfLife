d967820cc10853ffebe5b81d60d40fb5e4b90d2e
Mouad El abbade <elabbademouad@gmail.com>
2019-11-30 04:00:20 +0100

    implement getPointFromPixel and some fixing

--- src/app/services/canvas.service.ts
+++ src/app/services/canvas.service.ts
@@ -1,16 +1,11 @@
 import { Injectable, ElementRef } from '@angular/core';
 import { zoomEnum } from '../model/zoom-enum'
 import { point } from '../model/point-model';
+import { Constants } from '../constants/constants';
 @Injectable({
   providedIn: 'root'
 })
 export class CanvasService {
-  private gridWidth: number;
-  private gridHeight: number;
-
-  private sceneWidth: number;
-  private sceneHeight: number;
-
   private gridContext: CanvasRenderingContext2D;
   private grid: ElementRef;
   private sceneContext: CanvasRenderingContext2D;
@@ -21,7 +16,7 @@ export class CanvasService {
   constructor() { }
   zoom(e: zoomEnum) {
     if (e === zoomEnum.zoomOut) {
-      if (this._drawingUnit > 2) {
+      if (this._drawingUnit > Constants.MIN_DRAWING_UNIT) {
         this._drawingUnit -= 1;
         this.drawGrid();
       }
@@ -32,48 +27,46 @@ export class CanvasService {
   }
   setupCanvas(grid: ElementRef, scene: ElementRef) {
     this.grid = grid;
-    let dpi = window.devicePixelRatio;
-    this.gridHeight = this.grid.nativeElement.height = this.grid.nativeElement.height * dpi;
-    this.gridWidth = this.grid.nativeElement.width = this.gridHeight;
+    this.grid.nativeElement.height = Constants.CANVAS_HEIGHT;
+    this.grid.nativeElement.width = Constants.CANVAS_WIDTH;
     this.gridContext = this.grid.nativeElement.getContext("2d");
 
     this.scene = scene;
-    this.sceneHeight = this.scene.nativeElement.height = this.gridHeight;
-    this.sceneWidth = this.scene.nativeElement.width = this.gridHeight;
+    this.scene.nativeElement.height = Constants.CANVAS_HEIGHT;
+    this.scene.nativeElement.width = Constants.CANVAS_WIDTH;
     this.sceneContext = this.scene.nativeElement.getContext("2d");
-    this._drawingUnit = 8;
+    this._drawingUnit = Constants.DEFAULT_DRAWING_UNIT;
     this.drawGrid();
   }
 
   private drawGrid() {
-    for (let x = 0; x < this.gridWidth; x += this._drawingUnit) {
-      for (let y = 0; y < this.gridWidth; y += this._drawingUnit) {
-        this.gridContext.fillStyle = "white"
+    for (let x = 0; x < Constants.CANVAS_WIDTH; x += this._drawingUnit) {
+      for (let y = 0; y < Constants.CANVAS_HEIGHT; y += this._drawingUnit) {
+        this.gridContext.fillStyle = Constants.DEFAULT_GRID_FILL_COLOR
         this.gridContext.fillRect(x, y, this._drawingUnit, this._drawingUnit);
-        this.gridContext.lineWidth = 0.09 * window.devicePixelRatio;
-        this.gridContext.strokeStyle = "black"
+        this.gridContext.strokeStyle = Constants.DEFAULT_GRID_STORK_COLOR
         this.gridContext.strokeRect(x, y, this._drawingUnit, this._drawingUnit);
       }
     }
   }
-  public getPoint(e: any): point {
-    let x = (e.layerX / this._drawingUnit).toString().split('.')[0];
-    let y = (e.layerY / this._drawingUnit).toString().split('.')[0];
+  public getPointFromPixel(xPixel: any,yPixel:any): point {
+    let x = (xPixel / this._drawingUnit).toString().split('.')[0];
+    let y = (yPixel / this._drawingUnit).toString().split('.')[0];
     let p = new point();
     p.x = Number(x);
     p.y = Number(y);
     return p;
   }
   public drawGeneration(generations: Array<point>) {
-    for (let x = 0; x < this.gridWidth; x += this._drawingUnit) {
-      for (let y = 0; y < this.gridWidth; y += this._drawingUnit) {
+    for (let x = 0; x < Constants.CANVAS_WIDTH; x += this._drawingUnit) {
+      for (let y = 0; y < Constants.CANVAS_HEIGHT; y += this._drawingUnit) {
         this.sceneContext.clearRect(x, y, this._drawingUnit, this._drawingUnit);
       }
     }
     generations.forEach(p => {
-      this.sceneContext.fillStyle = "green"
+      this.sceneContext.fillStyle = Constants.DEFAULT_SCENE_FILL_COLOR
       this.sceneContext.fillRect(p.x * this._drawingUnit, p.y * this._drawingUnit, this._drawingUnit, this._drawingUnit);
-      this.sceneContext.strokeStyle = "black"
+      this.sceneContext.strokeStyle = Constants.DEFAULT_SCENE_STORK_COLOR
       this.sceneContext.strokeRect(p.x * this._drawingUnit, p.y * this._drawingUnit, this._drawingUnit, this._drawingUnit);
     })
   }
