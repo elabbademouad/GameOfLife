1ac469a8aa38cb0de38dfe4075061f1a4de3607a
Mouad El abbade <elabbademouad@gmail.com>
2019-11-30 03:57:31 +0100

    handle touch event and speed

--- src/app/play/play.page.ts
+++ src/app/play/play.page.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { CanvasService } from '../services/canvas.service';
 import { zoomEnum } from '../model/zoom-enum';
 import { GameEngineService } from '../services/game-engine.service';
 import { gameStateEnum } from '../model/game-state-enum';
+import { Constants } from '../constants/constants';
+import { point } from '../model/point-model';
 
 @Component({
   selector: 'app-play',
@@ -11,6 +13,8 @@ import { gameStateEnum } from '../model/game-state-enum';
 })
 export class PlayPage implements OnInit {
 
+  private _touchStartPoint:point=null;
+  private _touchEndPoint:point=null;
   public state: gameStateEnum = gameStateEnum.stoped;
   public generation: number = 0;
 
@@ -21,7 +25,7 @@ export class PlayPage implements OnInit {
   scene: ElementRef;
 
   constructor(public _canvasService: CanvasService,
-              public _gameEngineService: GameEngineService) {
+    public _gameEngineService: GameEngineService) {
     this._gameEngineService.getCurrentState()
       .subscribe((state: gameStateEnum) => {
         this.state = state;
@@ -30,18 +34,28 @@ export class PlayPage implements OnInit {
       .subscribe(generation => {
         this.generation = generation;
       });
-    
+
   }
 
   zoomClick(e: zoomEnum) {
     this._canvasService.zoom(e);
   }
 
+  touchstart(event:any){
+    let x=event.touches[0].clientX;
+    let y=event.touches[0].clientY;
+    this._touchStartPoint=this._touchStartPoint=this._canvasService.getPointFromPixel(x,y);
+  }
+  touchend(event:any){
+    let x=event.changedTouches[0].clientX;
+    let y=event.changedTouches[0].clientY;
+    
+    this._touchEndPoint=this._canvasService.getPointFromPixel(x,y);
+    this._gameEngineService.moveScene(this._touchStartPoint,this._touchEndPoint);
+  }
+
   ngOnInit() {
     this._canvasService.setupCanvas(this.grid, this.scene);
-    this._gameEngineService.addPoint({ x: 8, y: 7 });
-    this._gameEngineService.addPoint({ x: 8, y: 8 });
-    this._gameEngineService.addPoint({ x: 8, y: 9 });
   }
 
   public breakClick() {
@@ -56,9 +70,13 @@ export class PlayPage implements OnInit {
     this._gameEngineService.run();
   }
 
-  public sceneClick(e:any){
-    let point= this._canvasService.getPoint(e);
+  public sceneClick(e: any) {
+    let point = this._canvasService.getPointFromPixel(e.layerX,e.layerY);
     this._gameEngineService.addPoint(point);
-    //this._canvasService.drawGeneration()
+  }
+
+  public changeSpeed(event: any) {
+    let value = event.detail.value;
+    this._gameEngineService.changeSpeed(Constants.MAX_SPEED - value);
   }
 }
